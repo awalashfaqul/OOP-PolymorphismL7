ccb52db8de2bcf7fb1137d2a88202a383b088067
Ashfaqul Awal <ashfaq.awal@gmail.com>
2023-11-10 06:59:58 +0100

    Created RectangleGeometry class
    RectangleGeometry is a child class which inherits properties from parent class 'Geometry'.

---
+++ RectangleGeometry.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OOP_PolymorphismL7
+{
+    class RectangleGeometry : Geometry
+    {
+        public double L { get; set; }
+        public double W { get; set; }
+
+        public RectangleGeometry(double length, double width)
+        {
+            L = length;
+            W = width;
+        }
+
+        public override double area()
+        {
+            return L * W;
+        }
+    }
+}
+
