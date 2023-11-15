3ab67f7a6c55c1bece7b36be8df4b1241c03a845
Ashfaqul Awal <ashfaq.awal@gmail.com>
2023-11-15 06:30:19 +0100

    changing variable name formats to pasCal format

--- CircleGeometry.cs
+++ CircleGeometry.cs
@@ -4,19 +4,19 @@ namespace OOP_PolymorphismL7
     class CircleGeometry : Geometry
     {
         // properties for circle
-        public double Radius { get; set; }
+        public double crclRadius { get; set; }
 
         // constructor to create circle object
-        public CircleGeometry(double radius)
+        public CircleGeometry(double raDius)
         {
-            Radius = radius;
+            crclRadius = raDius;
         }
 
         // method to calculate area of the circle, where the area method
         // is overriden from parent Geometry
-        public override double area()
+        public override double geometricArea()
         {
-            return Math.PI * Radius * Radius;
+            return Math.PI * crclRadius * crclRadius;
         }
     }
 }
